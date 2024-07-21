ae6c783cacf642131735ca07e5fe3546f13aec8e
sarthakdev143-lite <sarthakdev143.official@gmail.com>
2024-07-21 20:44:54 +0530

    "Updated max-width to width in nav styles"

---
+++ src/Components/Case_Studies/Case_Studies.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Case_Studies = () => {
+    return (
+        <>
+            <section id='case-studies' className='case-studies'>
+                <h1>
+                    Case Studies
+                    <button>
+                        all case
+                        <span className="material-symbols-outlined">arrow_outward</span>
+                    </button>
+                </h1>
+            </section>
+        </>
+    )
+}
+
+export default Case_Studies
