9dde2acf68f6d5d55381e77d576d0ecdea0472e8
sarthakdev143-lite <sarthakdev143.official@gmail.com>
2024-08-08 20:52:02 +0530

    "Added imports and components to Review.jsx, replaced existing content with ReusableLanding, ClientReviews, and QuesAnsChild components"

--- src/Components/Review/Review.jsx
+++ src/Components/Review/Review.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import ReusableLanding from '../ReusableLanding/ReusableLanding'
+import bgImage from '../CaseStudies/imgs/caseStudies.jpg';
+import ClientReviews from '../Home/ClientReviews/ClientReviews';
+import QuesAnsChild from '../Industries/SubCompo/QuesAns/QuesAnsChild';
 
 const Review = () => {
+  const landingData = {
+    bgImage,
+    title: 'Our Reviews',
+  }
+
+  const questions = [
+    { question: "What is custom software development?", answer: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur.", },
+    { question: "Why should I choose custom software over off-the-shelf solutions?", answer: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur.", },
+    { question: "How long does it take to develop custom software?", answer: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur.", },
+    { question: "How do you ensure the security of custom software?", answer: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur.", },
+    { question: "How do you handle software updates and maintenance?", answer: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur.", },
+  ];
+
   return (
-    <div>Review</div>
+    <>
+      <ReusableLanding
+        bgImage={landingData.bgImage}
+        title={landingData.title}
+        showSlider={true}
+      />
+      <ClientReviews />
+      <QuesAnsChild questions={questions} />
+    </>
   )
 }
 
